f11f17506a9fd7f2339ad930470b98e0d065c12c
felipe_beck <felipe_beck@SNFNS2EP04VM175.florianopolis.sc.senai.br>
2014-06-04 22:06:48 -0300

    controller

---
+++ src/br/com/bartender/controller/ComandaController.java
@@ -0,0 +1,71 @@
+package br.com.bartender.controller;
+
+import br.com.bartender.model.Comanda;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author felipe_beck
+ */
+public class ComandaController {
+    
+    private static ComandaController instanciaRep;
+    
+    public static ComandaController obterInstancia(){
+        if ( instanciaRep == null ){
+            instanciaRep = new ComandaController();
+        }
+        return instanciaRep;
+    }
+    
+    public void Cadastrar(Comanda comanda) throws Exception{
+        
+        if (comanda.getIdComanda().equals("") ){
+            throw new Exception("Id inválido.");
+        }
+        if (comanda.getNomeClienteComanda().isEmpty() ){
+            throw new Exception("Nome do cliente inválido.");
+        }
+        if ( comanda.getTelefoneClienteComanda().isEmpty() ){
+            throw new Exception("Telefone inválido.");
+        }
+        if ( comanda.getSexoClienteComanda().isEmpty() ){
+            throw new Exception("Sexo inválido.");
+        }
+        ComandaController.obterInstancia().Cadastrar(comanda);
+    }
+    
+    public ArrayList<Comanda> listarTodos(){
+        return ComandaController.obterInstancia().listarTodos();
+    }
+    
+    public void Editar(Comanda comanda) throws Exception{
+        if ( comanda == null ){
+            throw new Exception("Selecione um produto");
+        }
+        if ( comanda.getIdComanda().equals("") ){
+            throw new Exception("Favor inserir o id da comanda.");        
+        }
+        if (comanda.getNomeClienteComanda().isEmpty() ){
+            throw new Exception("Favor inserir o nome.");
+        }
+        if ( comanda.getTelefoneClienteComanda().isEmpty() ){
+            throw new Exception("Favor inserir o telefone.");
+        }
+        if ( comanda.getSexoClienteComanda().equals("") ){
+            throw new Exception("Favor inserir o sexo.");
+        }
+        
+        ComandaController.obterInstancia().Editar(comanda);
+    
+    
+    }
+    
+    public void Excluir(Comanda comanda) throws Exception{
+        if ( comanda == null){
+            throw new Exception("Selecione um produto");
+        }
+        ComandaController.obterInstancia().Excluir(comanda);
+    }
+    
+}
